99696d137b1262abf39ee9b48c976c98c9bcfd64
ufkesba <brentufkes@hotmail.com>
2017-11-19 22:52:37 -0500

    Adding two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
