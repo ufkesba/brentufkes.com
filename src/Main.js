7f5d5dda38ec8a8927fee273b7890b09c92e4df6
ufkesba <brentufkes@hotmail.com>
2018-05-21 21:46:27 -0700

    Starting Color Changes

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends Component {
       return (
         <HashRouter>
           <div>
-            <h1>Simple SPA</h1>
+            <h1>Welcome to my Resume!</h1>
             <ul className="header">
               <li><NavLink to="/">Home</NavLink></li>
               <li><NavLink to="/stuff">Stuff</NavLink></li>
