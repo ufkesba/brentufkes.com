c58db7ea6cf3ce1dc4b9e646b810ec0a33660603
ufkesba <brentufkes@hotmail.com>
2018-05-27 20:15:20 -0700

    fixing filepaths

---
+++ src/home/home.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        <p>Page Under Construction</p>
+        
+      </div>
+    );
+  }
+}
+ 
+export default Home;
