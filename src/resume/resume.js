ad27c3673c71485069838c69651f098427704626
ufkesba <brentufkes@hotmail.com>
2018-06-04 22:45:11 -0700

    PDF

--- src/resume/resume.js
+++ src/resume/resume.js
@@ -3,11 +3,8 @@ import React, { Component } from "react";
 class resume extends Component {
   render() {
     return (
-      <div>
-        <h2>Resume</h2>
-        <object data="brentufkes.com/static/Resume.pdf?embed=true" type="application/pdf">
-          <embed src="brentufkes.com/static/Resume.pdf?embed=true" type="application/pdf"/>
-        </object> 
+      <div className="iFramePDF">
+          <iframe src="https://drive.google.com/file/d/0BxMnqIKkZJmESUZvUms4MDcxRWs/preview" height="100%" width="100%" frameBorder="0"  />
       </div>
     );
   }
